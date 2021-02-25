da438797dcd3ddc769f86b05ce26004513d450f1
Camilo Cely <59616536+Cuadao@users.noreply.github.com>
2021-02-24 21:25:16 -0500

    24/feb final upload assigment

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*GET about page.*/
+router.get('/about', (req, res, next) => {
+  res.render('about', {
+    title: 'about site',
+    pagetext: 'test test'
+  })
+})
+
 module.exports = router;
